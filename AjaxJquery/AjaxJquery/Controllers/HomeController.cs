14471c24dfd2922c1792fe5e7a1a0dd32f43c3e3
chechle39 <chechle3979@gmail.com>
2017-09-25 03:51:42 +0700

    ajax
    ajax

---
+++ AjaxJquery/AjaxJquery/Controllers/HomeController.cs
@@ -0,0 +1,125 @@
+﻿using AjaxDaTa.Model;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace AjaxJquery.Controllers
+{
+    public class HomeController : Controller
+    {
+        private EmployeeEntities _context;
+        public HomeController()
+        {
+            _context = new EmployeeEntities();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public JsonResult LoadData(int page , int pageSize =3) // int page,pagezSize phaan trang
+        {
+            var model = _context.Employees.OrderByDescending(x=>x.Name).Skip((page - 1) * pageSize).Take(pageSize); // skip lấy từ vị trí nào
+            int totalRow = _context.Employees.Count(); // tính tổn số trang
+            return Json(new
+            {
+                //data = list,
+                data = model,
+                total = totalRow,
+                Status = true
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult Update (string model)
+        {
+            JavaScriptSerializer serializer = new JavaScriptSerializer(); // chuyen doi json
+            Employees empl = serializer.Deserialize<Employees>(model); // mep ping
+            bool status = false;
+            string message = string.Empty;
+
+            // add new employee if id=0
+
+            if (empl.ID == 0)
+            {
+                empl.CreatedDate = DateTime.Now;
+                _context.Employees.Add(empl);
+
+                try
+                {
+                    _context.SaveChanges();
+                    status = true;
+                }
+                catch (Exception ex)
+                {
+                    status = false;
+                    message = ex.Message;
+                }
+            }
+            else
+            {
+                // cập nhật Db hiện có
+                // save database
+                var entity = _context.Employees.Find(empl.ID);
+                entity.Salary = empl.Salary;
+                entity.Name = empl.Name;
+                entity.Status = empl.Status;
+                try
+                {
+                    _context.SaveChanges();
+                    status = true;
+                }
+                catch (Exception ex)
+                {
+                    status = false;
+                    message = ex.Message;
+                }
+            }
+           
+            return Json(new
+            {
+                status = true
+            });
+        }
+
+        public JsonResult SaveData (string strempl)
+        {
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            Employees empl = serializer.Deserialize<Employees>(strempl);
+            bool status = false;
+            string message = string.Empty;
+            if (empl.ID == 0)
+            {
+                empl.CreatedDate = DateTime.Now;
+                _context.Employees.Add(empl);
+                _context.SaveChanges();
+                status = true;
+            }
+            else
+            {
+                // save db
+                var entity = _context.Employees.Find(empl.ID);
+                entity.Salary = empl.Salary;
+                entity.Name = empl.Name;
+                entity.Status = entity.Status;
+                try
+                {
+                    _context.SaveChanges();
+                    status = true;
+                }
+                catch (Exception ex)
+                {
+                    status = false;
+                    message = ex.Message;
+                }
+            }
+
+            return Json(new
+            {
+                status = status,
+                message = message
+            });
+        }
+    }
+}
---
+++ AjaxJquery/AjaxJquery/Scripts/controller/homeController.js
@@ -0,0 +1,151 @@
+﻿var homeconfig = {
+    pageSize: 20,
+    pageIndex : 1,
+}
+var homeController = {
+    init: function () {
+        homeController.loadData();
+        homeController.registerEvent();
+       
+
+    },
+    registerEvent: function () {
+        $('.txtSalary').off('keypress').on('keypress', function (e) {
+            if (e.which == 13) // kiem tra bam nut enter
+            {
+                var id = $(this).data('id');
+                var value = $(this).val();
+
+                homeController.updateSalary(id, value);
+            }
+        });
+
+
+
+        $('#btnAddnew').off('click').on('click',function(){
+            $('#modalAddUpdate').modal('show');
+            homeController.resetForm();
+        });
+
+        $('#btnSave').off('click').on('click',function(){
+            homeController.saveData();
+           
+        });
+    },
+
+    resetForm : function(){  //  xóa tất cả control
+        $('#hidID').val('0');
+        $('#txtName').val('');
+        $('txtSalary').val(0);
+        $('#ckStatus').prop('checked', true);
+    },
+
+    saveData:function(){
+        var name = $('#txtName').val();
+        var salary = parseFloat($('#txtSalary').val());
+        var status = $('#ckStatus').prop('checked');
+        var id = parseInt($('#hidID').val());
+        var  empl= {
+            Name: name,
+            Salary: salary,
+            Status:status,
+            ID:id
+        }
+        $.ajax({
+            url: '/Home/SaveData',
+            data: {
+                strempl: JSON.stringify(empl)
+            },
+            type: 'POST',
+            dataType: 'json',
+            success: function (rp) {
+                if (status == true) {
+                    alert('Them thanh cong');
+                    $('#modalAddUpdate').modal('hide');
+                    homeController.loadData();
+                }
+                else {
+                    alert(rp.Message);
+                }
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        });
+    },
+
+
+    updateSalary:function(id,value){
+        var data = {
+            ID:id,
+            Salary:value
+        };
+        $.ajax({
+            url:'/Home/Update',
+            type:'POST',
+            dataType:'json',
+            data:{model:JSON.stringify(data)},
+                success:function(rp){
+                if(rp.status)
+                {
+                    alert('update thanh cong')
+                }else{
+                    alert('update that bai');
+                }
+            }
+        })
+    },
+
+    loadData: function () {
+        $.ajax({
+            url: '/Home/LoadData',
+            type: 'GET',
+            data:{// phân trang
+                // page:1,
+                page:homeconfig.pageIndex,
+                pageSize:homeconfig.pageSize
+            },
+            dataType: 'json',
+            success: function (rp) {
+                if (rp.Status) {
+                    var data = rp.data;
+
+                    var html = '';
+                    var template = $('#data-template').html();
+                    $.each(data, function (i, item) {
+                        html += Mustache.render(template, {
+                            ID: item.ID,
+                            Name: item.Name,
+                            Salary: item.Salary,
+                            Status: item.Status==true? "<span class=\"label label-success\">Kich Hoat</span>":"<span class=\"label label-danger\">Khoa</span>"
+                        });
+                    });
+                    $('#tblData').html(html);
+                    homeController.paging(rp.total, function () {  // phân trang
+                        homeController.loadData();
+                    });
+                    homeController.registerEvent();
+                }
+            }
+        })
+    },
+
+    // phân trang
+    paging: function (totalRow, callback) {
+        var totalPage = Math.ceil(totalRow / homeconfig.pageSize);
+
+        $('#pagination').twbsPagination({
+            totalPages: totalPage,
+            first: "Đầu",
+            next: "Tiếp",
+            last: "Cuối",
+            prev:"Trước",
+            visiblePages: 10,
+            onPageClick: function (event, page) {
+                homeconfig.pageIndex=page,
+                setTimeout(callback, 200);
+            }
+        });
+    }
+}
+homeController.init();
